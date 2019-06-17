d9211cfbd41320ef84b2ec3b18d661acc7ab493b
jonathan weinstein <jonathanweinstein@jonathans-MBP.home>
2019-06-17 01:46:08 -0400

    seed data

---
+++ db/seeds.rb
@@ -0,0 +1,23 @@
+User.create([
+    { name: 'Jon', size: 'M', age: 31},
+    { name: 'A', size: 'L', age: 22},
+    { name: 'B', size: 'XL', age: 31},
+    { name: 'C', size: 'S', age: 25},
+    { name: 'D', size: 'XL', age: 44},
+  ])
+  
+  Item.create([
+    { style: 'shirt', size: 'S', color: 'blue', price: 10, user_id: 1},
+    { style: 'shirt', size: 'M', color: 'red', price: 12, user_id: 1},
+    { style: 'shirt', size: 'S', color: 'yellow', price: 10, user_id: 1},
+    { style: 'shirt', size: 'S', color: 'blue', price: 10, user_id: 2},
+    { style: 'shirt', size: 'L', color: 'red', price: 12, user_id: 2},
+    { style: 'shirt', size: 'S', color: 'yellow', price: 10, user_id: 2},
+    { style: 'shirt', size: 'XL', color: 'orange', price: 13, user_id: 2},
+    { style: 'shirt', size: 'M', color: 'red', price: 12, user_id: 2},
+    { style: 'shirt', size: 'S', color: 'pink', price: 10, user_id: 2},
+    { style: 'hat', size: 'S', color: 'yellow', price: 10, user_id: 3}
+  ])
+   
+    
+  
